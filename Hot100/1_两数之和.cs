c5398d5576377c9a45baf3ff74f7dc1d95634b2a
cnzoran <100952036+cnzoran@users.noreply.github.com>
2022-03-20 20:31:48 +0800

    1. 两数之和

---
+++ Hot100/1_两数之和.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Hot100
+{
+    //https://leetcode-cn.com/problems/two-sum/
+
+    class _1_两数之和
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int curValue = nums[i];
+
+                if (dic.ContainsKey(target - curValue))
+                {
+                    return new int[] { dic[target - curValue], i };
+                }
+
+                dic.Add(curValue, i);
+            }
+
+            return new int[2];
+        }
+
+        public void Test()
+        {
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int[] ans = TwoSum(nums, 9);
+            Console.WriteLine(string.Join(",", ans));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LeetCode.Hot100;
+using System;
 
 namespace LeetCode
 {
@@ -6,7 +7,9 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new _1_两数之和().Test();
+
+            Console.Read();
         }
     }
 }
