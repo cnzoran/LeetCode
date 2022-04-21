d6c24838b90b0be4ba735aa16a948c20a7e8ee45
cnzoran <100952036+cnzoran@users.noreply.github.com>
2022-04-22 01:45:26 +0800

    _75_颜色排序

---
+++ Hot100/75_颜色排序.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Hot100
+{
+    //https://leetcode-cn.com/problems/sort-colors/
+    internal class _75_颜色排序
+    {
+        public void SortColors(int[] nums)
+        {
+            (int left, int right) = (0, nums.Length - 1);
+            for (int i = 0; i <= right; i++)
+            {                
+                while (i <= right && nums[i] == 2)
+                {
+                    nums[i] = nums[right];
+                    nums[right--] = 2;
+                }
+                if (nums[i] == 0)
+                {
+                    nums[i] = nums[left];
+                    nums[left++] = 0;
+                }
+            }
+        }
+
+        public void Test()
+        {
+            int[] nums = { 2, 0, 1 };
+            SortColors(nums);
+            Console.WriteLine(string.Join(",", nums));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LeetCode
         static void Main(string[] args)
         {
             //_ = AwaitOperator.Main();
-            new _64_最小路径和().Test();
+            new _75_颜色排序().Test();
 
             Console.Read();
         }
