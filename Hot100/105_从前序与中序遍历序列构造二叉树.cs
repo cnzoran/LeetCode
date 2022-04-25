fd7c9f575682b9283adfddb71fcd0b0662963cdb
cnzoran <100952036+cnzoran@users.noreply.github.com>
2022-04-26 01:51:35 +0800

    _105_从前序与中序遍历序列构造二叉树 模板提交

---
+++ Hot100/105_从前序与中序遍历序列构造二叉树.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Hot100
+{
+    //https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+    internal class _105_从前序与中序遍历序列构造二叉树
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder.Length == 0)
+                return null;
+            BuildTree(preorder, 0, preorder.Length - 1, inorder, 0, inorder.Length - 1);
+        }
+
+        private TreeNode BuildTree(int[] preorder, int currPreorderLen, int preorderLen, int[] inorder, int currInorderLen, int inorderLen)
+        {
+            
+        }
+    }
+}
