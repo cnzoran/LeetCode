f020add74e19c6b58afc732ad472328b0ab7d124
cnzoran <100952036+cnzoran@users.noreply.github.com>
2022-05-04 23:33:48 +0800

    _139_单词拆分

---
+++ Hot100/139_单词拆分.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Hot100
+{
+    //https://leetcode-cn.com/problems/word-break/
+    public class _139_单词拆分
+    {
+        public bool WordBreak(string s, IList<string> wordDict)
+        {
+            if (string.IsNullOrEmpty(s) || wordDict == null || wordDict.Count == 0)
+                return false;
+
+            bool[] dp = new bool[s.Length + 1];
+            dp[0] = true;
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (dp[j] && wordDict.Contains(s.Substring(j, i - j)))
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.Length];
+        }
+    }
+}
